2814e763c14612b8d0cb4fbbd3b95c29ee608ca2
albanykt993 <mimusa993@gmail.com>
2025-04-29 02:11:15 +0600

    Mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const User = require("./model/model.User");
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World');
 })
 
 app.listen(4000, ()=>{
     console.log("Server is running on port 4000");
-})
+});
+
+mongoose.connect("mongodb+srv://<db_username>:<db_password>@cluster0.sirbea9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() =>{
+    console.log("Connected to MongoDB");
+}).catch((err) =>{
+    console.log("Failed to connect to MongoDb", err);
+});
+
+
+// Ib@08165959764
